fc8d441faa08104f33b5f9d4cb25b396190da01f
KevinDS-U <rodiguezk18@gmail.com>
2025-07-21 12:22:45 -0600

    panelcentral2.0

--- Banco_Digital/src/PanelCentral.java
+++ Banco_Digital/src/PanelCentral.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class PanelCentral {
+    private JTable Cuentas;
+    private JLabel clientenom;
+    private JLabel Saludo;
+    private JLabel inicio;
+    private JLabel icoinc;
 }
