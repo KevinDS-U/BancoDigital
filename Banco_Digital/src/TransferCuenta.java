8865c21fb8ee95dbf2ba23131f03a5a7a3142df6
KevinDS-U <rodiguezk18@gmail.com>
2025-07-22 11:48:22 -0600

    Panttalla Trasfer
    este es la estructura de  la pantalla de tranferir

--- Banco_Digital/src/TransferCuenta.java
+++ Banco_Digital/src/TransferCuenta.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+
 public class TransferCuenta {
+    private JPanel TRANFER;
+    private JComboBox TipoTrans;
+    private JPanel p1;
+    private JLabel tex1;
+    private JLabel tex2;
+    private JComboBox comboBox1;
+    private JLabel origen;
+    private JComboBox comboBox2;
+    private JLabel Destino;
+    private JTextField monto;
+    private JLabel labelmonto;
+    private JTextField saldorigen;
+    private JLabel NumCuentads;
+    private JTextField numcd;
+    private JButton enviarBoton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
